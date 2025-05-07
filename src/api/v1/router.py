977ef5668fcf3c97368972d7ea82a043dea914a4
Coke <jock74608@gmail.com>
2025-05-07 10:13:44 +0800

    fix(router): remove print and add verify permission.

--- src/api/v1/router.py
+++ src/api/v1/router.py
@@ -5,19 +5,24 @@ Author  : Coke
 Date    : 2025-04-23
 """
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from src.core.route import BaseRoute
-from src.deps.role import VerifyPermissionDep
+from src.deps.role import verify_user_permission
 from src.deps.router import RouterCrudDep
 from src.schemas.response import Response
 from src.schemas.router import FastAPIRouterResponse
 
-router = APIRouter(prefix="/router", tags=["Router"], route_class=BaseRoute)
+router = APIRouter(
+    prefix="/router",
+    tags=["Router"],
+    route_class=BaseRoute,
+    dependencies=[Depends(verify_user_permission)],
+)
 
 
 @router.get("/backend")
-async def get_router(route: RouterCrudDep, user: VerifyPermissionDep) -> Response[list[FastAPIRouterResponse]]:
+async def get_router(route: RouterCrudDep) -> Response[list[FastAPIRouterResponse]]:
     """
     This endpoint retrieves all routes from the database and returns them in the response.\f
 
@@ -28,6 +33,6 @@ async def get_router(route: RouterCrudDep, user: VerifyPermissionDep) -> Respons
         Response[list[FastAPIRouterResponse]]: A list of FastAPIRouterResponse models, which are
                                                 validated and returned as a response.
     """
-    print(user, "1233213")
+
     routes = await route.get_all()
     return Response(data=[FastAPIRouterResponse.model_validate(_route) for _route in routes])
